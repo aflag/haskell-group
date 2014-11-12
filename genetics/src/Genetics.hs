41f2cac9630dd54c8a40fca11727a914071a2452
Rafael Cunha de Almeida <rafael.cunha@corp.globo.com>
2014-11-12 21:17:59 -0200

    desenhamos o grid :)

--- genetics/src/Genetics.hs
+++ genetics/src/Genetics.hs
@@ -1,7 +1,23 @@
 module Genetics where
 
 import Graphics.Gloss
+import Data.Array
 
-step _ time model = 5*time + model
+step _ time model = model
 
-window = InWindow "Genetics" (800, 640) (100, 100)
+data Square = Open | Closed | Goal
+
+data World = World {
+    antPosition :: (Int, Int),
+    grid :: Array (Int, Int) Square
+}
+
+worldToPicture :: World -> Picture
+worldToPicture (World antPosition g) = pictures (map positionToPicture (assocs g))
+  where
+   positionToPicture ((x, y), v) = translate ((fromIntegral x)*20) ((fromIntegral y)*20) (color (squareToColor v) (rectangleSolid 20 20))
+   squareToColor (Open) = white
+   squareToColor (Closed) = black
+   squareToColor (Goal) = blue
+
+window = InWindow "Genetics" (600, 600) (100, 100)
--- genetics/src/Main.hs
+++ genetics/src/Main.hs
@@ -2,6 +2,9 @@ module Main where
 
 import Genetics
 import Graphics.Gloss
+import Data.Array
 
 
-main = simulate window white 10 5 circle step
+model = World (0,0) (array ((-1,-1), (1,1)) [((-1,-1), Closed), ((-1, 0), Goal), ((-1, 1), Open), ((0,-1), Closed), ((0,0), Open), ((0,1), Closed), ((1,-1), Open), ((1,0), Open), ((1,1), Closed)])
+
+main = simulate window white 10 model worldToPicture step
