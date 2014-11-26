78a9ef0fc7ebe3333dd1f4e1f51d160e741a4a9c
Rafael Cunha de Almeida <rafael.cunha@corp.globo.com>
2014-11-26 19:16:46 -0200

    Desenhando galinhas

--- genetics/src/Genetics.hs
+++ genetics/src/Genetics.hs
@@ -3,21 +3,31 @@ module Genetics where
 import Graphics.Gloss
 import Data.Array
 
-step _ time model = model
+step _ _ (World chickens g) = World (map (\(x,y)->(x-1, y)) chickens) g
 
-data Square = Open | Closed | Goal
+data Square = Open | Closed | Goal | Start
 
-data World = World {
-    antPosition :: (Int, Int),
-    grid :: Array (Int, Int) Square
-}
+type Grid = Array (Int, Int) Square
+type Chicken = (Int, Int)
+
+data World = World [Chicken] Grid
 
 worldToPicture :: World -> Picture
-worldToPicture (World antPosition g) = pictures (map positionToPicture (assocs g))
+worldToPicture (World chickens g) = pictures (worldPictures ++ chickenPictures)
   where
-   positionToPicture ((x, y), v) = translate ((fromIntegral x)*20) ((fromIntegral y)*20) (color (squareToColor v) (rectangleSolid 20 20))
-   squareToColor (Open) = white
-   squareToColor (Closed) = black
-   squareToColor (Goal) = blue
+    chickenPictures = map chickenToPicture chickens
+    chickenToPicture (x, y) =
+      translate ((fromIntegral x)*20) ((fromIntegral y)*20) $
+      color chartreuse $
+      circleSolid 5
+    worldPictures = map cellToPicture (assocs g)
+    cellToPicture ((x, y), v) =
+      translate ((fromIntegral x)*20) ((fromIntegral y)*20) $
+      color (squareToColor v) $
+      rectangleSolid 20 20
+    squareToColor Open = white
+    squareToColor Closed = black
+    squareToColor Goal = blue
+    squareToColor Start = green
 
 window = InWindow "Genetics" (600, 600) (100, 100)
--- genetics/src/Main.hs
+++ genetics/src/Main.hs
@@ -5,6 +5,6 @@ import Graphics.Gloss
 import Data.Array
 
 
-model = World (0,0) (array ((-1,-1), (1,1)) [((-1,-1), Closed), ((-1, 0), Goal), ((-1, 1), Open), ((0,-1), Closed), ((0,0), Open), ((0,1), Closed), ((1,-1), Open), ((1,0), Open), ((1,1), Closed)])
+model = World [(0,0)] (array ((-1,-1), (1,1)) [((-1,-1), Closed), ((-1, 0), Goal), ((-1, 1), Open), ((0,-1), Closed), ((0,0), Open), ((0,1), Closed), ((1,-1), Open), ((1,0), Open), ((1,1), Closed)])
 
-main = simulate window white 10 model worldToPicture step
+main = simulate window white 1 model worldToPicture step
